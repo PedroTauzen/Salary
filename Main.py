dcd133c54659f0242aca5e05f0e402402d62bbc2
Pedro Tauzen <pedrotauzen@gmail.com>
2024-12-27 15:20:50 +0000

    Eliminated the columns: "work_year", "salary", "salary_currency", "job_title"

--- Main.py
+++ Main.py
@@ -7,5 +7,8 @@ file_path = os.path.join("original_data", "jobs_in_data.csv")
 # Ler os dados do ficheiro CSV
 data = pd.read_csv(file_path)
 
-# Imprimir os primeiros registos
-print(data.head())
+# Eliminar as colunas irrelevantes
+columsn_to_drop = ["work_year", "salary", "salary_currency", "job_title"]
+data = data.drop(columns=columsn_to_drop)
+
+
