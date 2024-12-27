e8c9a466292f754035a2dab3c4e4aaaaff0236f5
Pedro Tauzen <pedrotauzen@gmail.com>
2024-12-27 15:36:20 +0000

    Quartile calculation was used to identify three salary classifications: high, medium, and low. Created a salary classification column: "salary_classification"

--- Main.py
+++ Main.py
@@ -1,14 +1,34 @@
 import pandas as pd
 import os
 
-# Caminho relativo para o ficheiro
+# Caminho relativo para o ficheiro.
 file_path = os.path.join("original_data", "jobs_in_data.csv")
 
 # Ler os dados do ficheiro CSV
 data = pd.read_csv(file_path)
 
-# Eliminar as colunas irrelevantes
+# Eliminar as colunas irrelevantes.
 columsn_to_drop = ["work_year", "salary", "salary_currency", "job_title"]
 data = data.drop(columns=columsn_to_drop)
 
+# Manter a coluna original de salário e criar uma coluna categorizada.
+salary_classification = "salary_in_usd"
 
+# Calcular os quartis
+q1 = data[salary_classification].quantile(0.25)
+q3 = data[salary_classification].quantile(0.75)
+
+# Definir as classes de salários com base nos quartis.
+def classify_salary(salary):
+    if salary <= q1:
+        return "low_salary"
+    elif q1 < salary <= q3:
+        return "medium_salary"
+    else:
+        return "high_salary"
+
+# Cria uma coluna para a classificação de salários.
+data["salary_classification"] = data[salary_classification].apply(classify_salary)
+
+# Imprimir header
+print(data.head())
